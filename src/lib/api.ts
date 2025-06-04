c11172d7be989f12b18d0b7b76f6be17964dc32d
Ankit Lakra <ankit.lakra@datapeace.in>
2025-06-04 12:30:17 +0530

    fix(libs/api): added type support

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,19 +1,38 @@
-export async function fetchFixtures(page = 1, limit = 5) {
+import { FixturesResponse } from "@/pages/api/fixtures";
+import { PointsTableResponse } from "@/pages/api/points-table";
+import { ResultsResponse } from "@/pages/api/results";
+
+export async function fetchFixtures(
+  page = 1,
+  limit = 5
+): Promise<FixturesResponse> {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/api/fixtures?page=${page}&limit=${limit}`
+    `${
+      process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+    }/api/fixtures?page=${page}&limit=${limit}`
   );
   return res.json();
 }
 
-export async function fetchResults(page?: number, limit?: number) {
-  let url = `${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/api/results`;
+export async function fetchResults(
+  page?: number,
+  limit?: number
+): Promise<ResultsResponse> {
+  let url = `${
+    process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+  }/api/results`;
   if (page && limit) url += `?page=${page}&limit=${limit}`;
   const res = await fetch(url);
   return res.json();
 }
 
-export async function fetchPointsTable(page?: number, limit?: number) {
-  let url = `${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/api/points-table`;
+export async function fetchPointsTable(
+  page?: number,
+  limit?: number
+): Promise<PointsTableResponse> {
+  let url = `${
+    process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+  }/api/points-table`;
   if (page && limit) url += `?page=${page}&limit=${limit}`;
   const res = await fetch(url);
   return res.json();
