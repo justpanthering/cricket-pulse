e9d260ed36f7c9f22718dd7b517810298944dbbb
Ankit Lakra <ankit.lakra@datapeace.in>
2025-06-06 13:34:49 +0530

    feat: added next-progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "@/components/Layout/Layout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import NextNProgress from "nextjs-progressbar";
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
+        <NextNProgress
+          color="#4299e1"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+        />
         <Component {...pageProps} />
       </Layout>
     </QueryClientProvider>
